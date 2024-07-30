8bd048fa493d2c9eabbb7a9b7f27f308df1395b3
Jorge Luiz <batistajoorge@gmail.com>
2024-07-29 21:48:43 -0300

    fix(estilo/textos): Adicionando um estilo mais amigável para o usuário

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-let nomeUsuario = prompt("Olá, porfavor, digite seu nome");
-let nomeDigitado = document.getElementsByClassName('primeiro_texto');
-if (nomeDigitado.length > 0) {
-    nomeDigitado[0].innerHTML = `Olá, ${nomeUsuario} ! <br> Seja bem-vindo ao Dev Class 2024!!!`;
-
+function digitarTexto(elemento, texto, velocidade) {
+    let i = 0;
+    function escrever() {
+        if (i < texto.length) {
+            elemento.innerHTML += texto.charAt(i);
+            i++;
+            setTimeout(escrever, velocidade);
+        }
+    }
+    escrever();
 }
+
+let nomeUsuario = prompt("Olá, por favor, digite seu nome");
+let nomeDigitado = document.getElementsByClassName('primeiro_texto');
+
+// Verificar se tem pelo menos um elemento 
+if (nomeDigitado.length > 0) {
+    let texto = `Olá, ${nomeUsuario}! Seja bem-vindo ao Dev Class 2024!!!`;
+    nomeDigitado[0].style.color = 'black';
+    // Efeito de digitação
+    digitarTexto(nomeDigitado[0], texto, 100); 
+}
+
+
